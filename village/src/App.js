14563d16811bc2f58c51e9b3a2b33ed8c4f9b365
maxgunter99 <maxgunter99@gmail.com>
2019-04-12 11:58:39 -0500

    more styling done, not enough time to add delete

--- village/src/App.js
+++ village/src/App.js
@@ -18,18 +18,19 @@ class App extends Component {
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then((response) => {
-      this.setState(() => ({smurfs: response.data}))
-    })
-    .catch(err => console.log(err))
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then((response) => {
+        this.setState(() => ({smurfs: response.data}))
+      })
+      .catch(err => console.log(err))
   }
   render() {
     return (
       <div className="App">
         <div className='navigation'>
           <NavLink className='nav' to= '/'>Home</NavLink>
-          <NavLink className = 'nav' to= '/smurf-form'>Create Smurf</NavLink>
+          <NavLink className = 'nav' to= '/smurf-form'>Home</NavLink>
         </div>
         <Route path = '/smurf-form' render={props => (
           <SmurfForm {...props} />)}
